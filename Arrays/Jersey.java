49ee317790f3ab499f7e67603c4fb7bbcc188485
Rishaan <91053141+rishn@users.noreply.github.com>
2024-04-14 15:56:09 +0530

    Update and rename TCSJersey.java to Jersey.java

--- Arrays/Jersey.java
+++ Arrays/Jersey.java
@@ -1,3 +1,15 @@
+/*
+Jack is a sports teacher at St. Patrick’s School. He makes games not only to make the student fit, but also smart. So, he lined up all the N number of students in his class. 
+At each position he has fixed a board with the integer number printed on it. Each of the numbers are unique and are in exactly the range of N. 
+Let us say there are 10 students, then the boards will be printed with numbers from 1 to 10 in a random order given by the sequence A[].
+
+As a rule, all students wear a jersey with their numbers printed on it. So, if there are N students, each will have a number, just like a football team.
+
+Now, in the beginning, all the students will stand as per the increasing order of their jersey numbers, from left to right.
+
+The only difference will be their respective board number which is placed at their respective location. The board location is fixed and cannot be changed. 
+*/
+
 import java.util.*;
 
 class Jersey {
